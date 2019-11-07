f824517e88f45253ef446c3a73f99d8eec95e2ab
psp <psp@adorsys.de>
2019-11-07 16:17:00 +0100

    DOC-284 fixed use0_6_1

--- datasafe-migration-shaded-0.6.1/src/test/java/de/adorsys/datasafe/migration/Use0_6_1.java
+++ datasafe-migration-shaded-0.6.1/src/test/java/de/adorsys/datasafe/migration/Use0_6_1.java
@@ -1,9 +1,9 @@
 package de.adorsys.datasafe.migration;
 
-import de.adorsys.datasafe_0_6_1.simple.adapter.api.types.DFSCredentials;
-import de.adorsys.datasafe_0_6_1.simple.adapter.impl.SimpleDatasafeServiceImpl;
+import de.adorsys.datasafe_0_6_1.simple.adapter.api.types.SO_DFSCredentials;
+import de.adorsys.datasafe_0_6_1.simple.adapter.impl.SO_SimpleDatasafeServiceImpl;
 
 public class Use0_6_1 {
-    private SimpleDatasafeServiceImpl imple;
-    private DFSCredentials credentials;
+    private SO_SimpleDatasafeServiceImpl imple;
+    private SO_DFSCredentials credentials;
 }
