3f60a7219309855e23e5a9fcb35b66b2b564d355
psp <psp@adorsys.de>
2019-11-07 16:08:56 +0100

    DOC-284 added travis.yml

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-mvn --settings .travis/settings.xml package gpg:sign deploy -Prelease -B -U;
+mvn --settings .travis/settings.xml package gpg:sign deploy -Prelease -DskipTests -B -U;
