d3fb49f92ea561ec337426eff0054ac1700d1c6e
psp <psp@adorsys.de>
2019-11-07 16:19:37 +0100

    DOC-284 removed use0_6_1

--- datasafe-migration-shaded-0.6.1/src/test/java/de/adorsys/datasafe/migration/Use0_6_1.java
+++
@@ -1,9 +0,0 @@
-package de.adorsys.datasafe.migration;
-
-import de.adorsys.datasafe_0_6_1.simple.adapter.api.types.SO_DFSCredentials;
-import de.adorsys.datasafe_0_6_1.simple.adapter.impl.SO_SimpleDatasafeServiceImpl;
-
-public class Use0_6_1 {
-    private SO_SimpleDatasafeServiceImpl imple;
-    private SO_DFSCredentials credentials;
-}
