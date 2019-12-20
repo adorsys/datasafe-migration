5e8519483054727fb5c27e84c24a377111358f3d
peter <peter@admins-MBP.fritz.box>
2019-12-20 08:17:45 +0100

    fixed import

--- datasafe-migration-silent/src/test/java/de/adorsys/datasafemigration/SimpleDocumentFQNTest.java
+++ datasafe-migration-silent/src/test/java/de/adorsys/datasafemigration/SimpleDocumentFQNTest.java
@@ -2,7 +2,7 @@ package de.adorsys.datasafemigration;
 
 import de.adorsys.datasafe.simple.adapter.api.types.DocumentDirectoryFQN;
 import de.adorsys.datasafe.simple.adapter.api.types.DocumentFQN;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SimpleDocumentFQNTest {
@@ -10,7 +10,7 @@ public class SimpleDocumentFQNTest {
     @Test
     public void testAddName() {
         DocumentFQN d = blobsFQN("id1");
-        Assert.assertEquals("/blobs/id1", d.getDocusafePath());
+        Assertions.assertEquals("/blobs/id1", d.getDocusafePath());
     }
 
     public static DocumentFQN blobsFQN(String blobId) {
